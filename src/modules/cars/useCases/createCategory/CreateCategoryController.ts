6d3fe63ade5e1b2deb6ab55b59e3731af356e044
Rodrigo Bussolo <rbussolo91@gmail.com>
2021-04-28 22:21:31 -0400

    Adicionado migração e entidade a aplicação.

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -5,12 +5,16 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 class CreateCategoryController {
     constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-    handle(request: Request, response: Response): Response {
+    async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
+        
+        try{
+            await this.createCategoryUseCase.execute({ name, description });
 
-        this.createCategoryUseCase.execute({ name, description });
-
-        return response.status(201).send();
+            return response.status(201).send();
+        }catch(err){
+            return response.status(400).json({ message: err.message });
+        }
     }
 }
 
